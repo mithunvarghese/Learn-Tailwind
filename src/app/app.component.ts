98af1af8949080b446650ff62e5470a429090a54
vargis.mithun <tonedeafclown@gmail.com>
2024-09-10 21:16:13 +0530

    Add cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Learn-Tailwind';
+  faCheck=faCheck
 }
